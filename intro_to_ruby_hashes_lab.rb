c0cfe27465a1643338339c750ffa5bdc4443246c
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-30 13:03:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash = {key => a_value}
+  hash = {key => value}
   if hash[:key]
     hash[:key] += 1 
   else hash[:key] = 1 
