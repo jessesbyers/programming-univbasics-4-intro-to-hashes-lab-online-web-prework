5dcc78f5da94a8bc8cbdc94a6de5361414e736cd
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-30 13:05:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ end
 
 def update_counting_hash(hash, key)
   hash = {key => a_value}
-  if hash[:key]
-    hash[:key] += 1 
-  else hash[:key] = 1 
+  if hash["key"]
+    hash["key"] += 1 
+  else hash["key"] = 1 
     end
     return hash
 end
